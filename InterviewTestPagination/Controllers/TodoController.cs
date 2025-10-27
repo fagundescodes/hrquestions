e60957edf042819dc831bedc8a9aac512f64792d
Raphael Fagundes <fagundesraphael7@gmail.com>
2025-10-27 10:01:47 -0400

    fix: pagination issues

--- InterviewTestPagination/Controllers/TodoController.cs
+++ InterviewTestPagination/Controllers/TodoController.cs
@@ -2,23 +2,73 @@
 using System.Web.Http;
 using InterviewTestPagination.Models;
 using InterviewTestPagination.Models.Todo;
+using System;
+using System.Linq;
+using InterviewTestPagination.Filter;
+using Newtonsoft.Json;
 
-namespace InterviewTestPagination.Controllers {
+namespace InterviewTestPagination.Controllers
+{
     /// <summary>
     /// 'Rest' controller for the <see cref="Todo"/>
     /// model.
     /// 
     /// TODO: implement the pagination Action
     /// </summary>
-    public class TodoController : ApiController {
+    public class TodoController : ApiController
+    {
 
         // TODO: [low priority] setup DI 
         private readonly IModelService<Todo> _todoService = new TodoService();
 
         [HttpGet]
-        public IEnumerable<Todo> Todos(/* parameters  */) {
-            return _todoService.Repository.All();
-        }
+        public IEnumerable<Todo> Todos(int pageNumber = 1, int pageSize = 10, string sortBy = "createdDate", string sortOrder = "desc")
+        {
+            var validFilter = new PaginationFilter(pageNumber, pageSize);
 
+            var allData = _todoService.Repository.All();
+
+            IEnumerable<Todo> sorted;
+            switch (sortBy.ToLower())
+            {
+                case "id":
+                    sorted = sortOrder.ToLower() == "asc"
+                        ? allData.OrderBy(t => t.Id)
+                        : allData.OrderByDescending(t => t.Id);
+                    break;
+                case "task":
+                    sorted = sortOrder.ToLower() == "asc"
+                        ? allData.OrderBy(t => t.Task)
+                        : allData.OrderByDescending(t => t.Task);
+                    break;
+                case "createddate":
+                default:
+                    sorted = sortOrder.ToLower() == "asc"
+                        ? allData.OrderBy(t => t.CreatedDate)
+                        : allData.OrderByDescending(t => t.CreatedDate);
+                    break;
+            }
+
+            var paged = sorted
+                .Skip((validFilter.PageNumber - 1) * validFilter.PageSize)
+                .Take(validFilter.PageSize)
+                .ToList();
+
+            var totalRecords = allData.Count();
+            var totalPages = (int)Math.Ceiling((double)totalRecords / (double)validFilter.PageSize);
+
+            var metaData = new
+            {
+                TotalCount = totalRecords,
+                PageSize = validFilter.PageSize,
+                CurrentPage = validFilter.PageNumber,
+                TotalPages = totalPages
+            };
+
+            System.Web.HttpContext.Current.Response.Headers.Add("X-Pagination",
+                Newtonsoft.Json.JsonConvert.SerializeObject(metaData));
+
+            return paged;
+        }
     }
 }
--- InterviewTestPagination/app/main.js
+++ InterviewTestPagination/app/main.js
@@ -1,65 +1,142 @@
-﻿(function (angular) {
-    "use strict";
+﻿(function(angular) {
+  "use strict";
 
-    angular
-        .module("todoApp")
-        .directive("todoPaginatedList", [todoPaginatedList])
-        .directive("pagination", [pagination]);
+  angular
+    .module("todoApp")
+    .directive("todoPaginatedList", [todoPaginatedList])
+    .directive("pagination", [pagination]);
 
-    /**
-     * Directive definition function of 'todoPaginatedList'.
-     * 
-     * TODO: correctly parametrize scope (inherited? isolated? which properties?)
-     * TODO: create appropriate functions (link? controller?) and scope bindings
-     * TODO: make appropriate general directive configuration (support transclusion? replace content? EAC?)
-     * 
-     * @returns {} directive definition object
-     */
-    function todoPaginatedList() {
-        var directive = {
-            restrict: "E", // example setup as an element only
-            templateUrl: "app/templates/todo.list.paginated.html",
-            scope: {}, // example empty isolate scope
-            controller: ["$scope", "$http", controller],
-            link: link
-        };
+  /**
+   * Directive definition function of 'todoPaginatedList'.
+   * 
+   * TODO: correctly parametrize scope (inherited? isolated? which properties?)
+   * TODO: create appropriate functions (link? controller?) and scope bindings
+   * TODO: make appropriate general directive configuration (support transclusion? replace content? EAC?)
+   * 
+   * @returns {} directive definition object
+   */
+  function todoPaginatedList() {
+    var directive = {
+      restrict: "E", // example setup as an element only
+      templateUrl: "app/templates/todo.list.paginated.html",
+      // scope: {}, // example empty isolate scope
+      controller: ["$scope", "$http", controller],
+      link: link
+    };
 
-        function controller($scope, $http) { // example controller creating the scope bindings
-            $scope.todos = [];
-            // example of xhr call to the server's 'RESTful' api
-            $http.get("api/Todo/Todos").then(response => $scope.todos = response.data);
+    function controller($scope, $http) { // example controller creating the scope bindings
+      $scope.todos = [];
+      $scope.pageNumber = 1;
+      $scope.pageSize = "20";
+      $scope.totalPages = 0;
+      $scope.totalItems = 0;
+
+      $scope.sortBy = "createdDate";
+      $scope.sortOrder = "desc";
+
+      $scope.loadTodos = function() {
+        $http.get("api/Todo/Todos", {
+          params: {
+            pageNumber: $scope.pageNumber,
+            pageSize: $scope.pageSize,
+            sortBy: $scope.sortBy,
+            sortOrder: $scope.sortOrder
+          }
+        }).then(function(response) {
+          $scope.todos = response.data;
+          var paginationHeader = response.headers('X-Pagination');
+          if (paginationHeader) {
+            var metadata = JSON.parse(paginationHeader);
+            $scope.totalPages = metadata.TotalPages;
+            $scope.totalItems = metadata.TotalCount;
+            $scope.pageNumber = metadata.CurrentPage;
+          }
+        });
+      };
+
+      $scope.sortColumn = function(column) {
+        if ($scope.sortBy === column) {
+          if ($scope.sortOrder === "asc") {
+            $scope.sortOrder = "desc";
+          } else {
+            $scope.sortOrder = "asc";
+          }
+        } else {
+          $scope.sortBy = column;
+          $scope.sortOrder = "asc";
         }
+        $scope.pageNumber = 1;
+        $scope.loadTodos();
+      };
 
-        function link(scope, element, attrs) { }
+      $scope.nextPage = function() {
+        if ($scope.pageNumber < $scope.totalPages) {
+          $scope.pageNumber++;
+          $scope.loadTodos();
+        }
+      };
 
-        return directive;
+      $scope.prevPage = function() {
+        if ($scope.pageNumber > 1) {
+          $scope.pageNumber--;
+          $scope.loadTodos();
+        }
+      };
+
+      $scope.firstPage = function() {
+        $scope.pageNumber = 1;
+        $scope.loadTodos();
+      };
+
+      $scope.lastPage = function() {
+        $scope.pageNumber = $scope.totalPages;
+        $scope.loadTodos();
+      };
+
+      $scope.changePageSize = function() {
+        $scope.pageNumber = 1;
+        $scope.loadTodos();
+      };
+
+      $scope.goToPage = function() {
+        if ($scope.pageNumber >= 1 && $scope.pageNumber <= $scope.totalPages) {
+          $scope.loadTodos();
+        }
+      };
+
+      $scope.loadTodos();
     }
 
-    /**
-     * Directive definition function of 'pagination' directive.
-     * 
-     * TODO: make it a reusable component (i.e. usable by any list of objects not just the Models.Todo model)
-     * TODO: correctly parametrize scope (inherited? isolated? which properties?)
-     * TODO: create appropriate functions (link? controller?) and scope bindings
-     * TODO: make appropriate general directive configuration (support transclusion? replace content? EAC?)
-     * 
-     * @returns {} directive definition object
-     */
-    function pagination() {
-        var directive = {
-            restrict: "E", // example setup as an element only
-            templateUrl: "app/templates/pagination.html",
-            scope: {}, // example empty isolate scope
-            controller: ["$scope", controller],
-            link: link
-        };
+    function link(scope, element, attrs) { }
 
-        function controller($scope) { }
+    return directive;
+  }
 
-        function link(scope, element, attrs) { }
+  /**
+   * Directive definition function of 'pagination' directive.
+   * 
+   * TODO: make it a reusable component (i.e. usable by any list of objects not just the Models.Todo model)
+   * TODO: correctly parametrize scope (inherited? isolated? which properties?)
+   * TODO: create appropriate functions (link? controller?) and scope bindings
+   * TODO: make appropriate general directive configuration (support transclusion? replace content? EAC?)
+   * 
+   * @returns {} directive definition object
+   */
+  function pagination() {
+    var directive = {
+      restrict: "E", // example setup as an element only
+      templateUrl: "app/templates/pagination.html",
+      // scope: {}, // example empty isolate scope
+      controller: ["$scope", controller],
+      link: link
+    };
 
-        return directive;
-    }
+    function controller($scope) { }
+
+    function link(scope, element, attrs) { }
+
+    return directive;
+  }
 
 })(angular);
 
